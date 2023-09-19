1ef44a77787be71df344ca2b8fb66b98564ea46a
Diego Scaravaggi <dskarava69@aol.com>
2023-09-19 11:02:23 +0200

    comment #1 add requisite for MS sqlserver

---
+++ compose/test_mssql.php
@@ -0,0 +1,11 @@
+<?php
+/* use correct address and password */
+$serverName = "172.20.12.258";
+$connectionInfo = array( "Database"=>"anagrafica", "UID"=>"php-anag-user", "PWD"=>"*******" );
+$con = sqlsrv_connect( $serverName, $connectionInfo);
+if( $con ) {
+    echo "connection established.<br />";
+}else{
+echo "connection could not be established.<br />";
+die( print_r( sqlsrv_errors(), true));
+}
